c57362e7d13e2edc2f8b60d80cd7c7fd027f0ef1
Santiago Latorre <s.latorre@uniandes.edu.co>
2023-04-14 18:10:56 -0500

    Creacion Segundo Thread

--- src/caso2/CargadeReferencias.java
+++ src/caso2/CargadeReferencias.java
@@ -7,25 +7,53 @@ public class CargadeReferencias extends Thread{
 	private ArrayList<String> paginasLista;
 	private Manejador mj;
 	private int cant;
+	private String tipo;
+	private boolean continuar;
 	
-	public CargadeReferencias(ArrayList<String>pags, Manejador mj) {
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public CargadeReferencias(ArrayList<String>pags, Manejador mj, String tipo) {
 		paginasLista = pags;
 		this.mj = mj;
+		this.tipo = tipo;
 	}
 	
 	public void run() {
 		mj.llenarMemoriaVirtual(paginasLista);
-		for (int i = 0; i < paginasLista.size(); i++) {
-			synchronized(mj) {
-			
-				mj.agregarPagina(Integer.valueOf(paginasLista.get(i)));
+		
+		
+			continuar = true;
+			if (this.getTipo().equals("cargador")){
+				for (int i = 0; i < paginasLista.size(); i++) {
+						
+						System.out.println("carg");
+						mj.agregarPagina(Integer.valueOf(paginasLista.get(i)));
+						try {
+							sleep(2);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					
+				}
+			continuar= false;
+		
+		} else if (this.getTipo().equals("envejecimiento")) {
+			for (int i = 0; i < paginasLista.size()*2; i ++) {
+				System.out.println("envej");
+				mj.envejecimiento(mj.getMarcoPagina());
 				try {
-					sleep(2);
+					sleep(1);
+					
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		System.out.println(mj.getFallas());
+			
+		System.out.println("Ocurrieron " + mj.getFallas() + " fallas");
+		
 	}
 }
+
